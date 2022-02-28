fc1f2b54345eaa8b9df88f9444884c0d7f5fa082
Garn123 <garnyg@hotmail.com>
2022-02-28 16:40:23 +0000

    Iteration2

--- src/JSBasicAlgorithms.js
+++ src/JSBasicAlgorithms.js
@@ -1,4 +1,30 @@
+//Iteration#1
+
 let myFavoriteHero = "Hulk";
 let x = 50;
 let h = 5, y = 10;
 let z = h + y;
+
+//Iteration#2
+
+const character = {name: 'Jack Sparrow', age: 25};
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos.`);
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+const precio = toy1['price'] + toy2['price'];
+
+console.log(precio);
+
+
+let globalBasePrice = 25000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: car1['baseprice'] + globalBasePrice};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: car2['baseprice'] + globalBasePrice};
+
+console.log(car1['finalPrice'], car2['finalPrice'])
+
