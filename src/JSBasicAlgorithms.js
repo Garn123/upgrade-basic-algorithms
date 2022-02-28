aaacfdc1ee467a284524b97a7fa6eeaf81bf0035
Garn123 <garnyg@hotmail.com>
2022-02-28 17:43:06 +0000

    Iteration#6

--- src/JSBasicAlgorithms.js
+++ src/JSBasicAlgorithms.js
@@ -63,7 +63,7 @@ console.log(rickAndMortyCharacters[4]); */
 
 //Iteration#5
 
-let number1 = 10;
+/* let number1 = 10;
 let number2 = 20;
 let number3 = 2;
 
@@ -89,4 +89,23 @@ if (number3*5 == number1 && number1*2 == number2) {
 
 if (number2/2 == number1 || number1/5 == number3) {
   console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+} */
+
+//Iteration#6
+/* for(let i = 0; i < 10; i++){
+    console.log(i);
+} */
+
+/* for(let i = 0; i < 10; i++){
+    if(i%2 == 0){
+        console.log(i)
+    }
+} */
+
+for(let i = 0; i <= 10; i++){
+    if(i == 10){
+        console.log("Dormido!")
+    }else{
+        console.log("intentando dormir");
+    }
 }
