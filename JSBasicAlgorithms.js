59cb56bb187836042e862c673944ad2114406070
Garn123 <garnyg@hotmail.com>
2022-03-01 14:04:00 +0000

    Iterations

---
+++ JSBasicAlgorithms.js
@@ -0,0 +1,117 @@
+//Iteration#1
+
+/* let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5, y = 10;
+let z = h + y; */
+
+//Iteration#2
+
+/* const character = {name: 'Jack Sparrow', age: 25};
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24; 
+
+console.log(`Soy ${firstName} ${lastName}, tengo ${age} y me gustan los lobos.`);
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+const precio = toy1['price'] + toy2['price'];
+
+console.log(precio);
+
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000;
+
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
+
+*/
+//Iteration#3
+
+/* let multiplication = 10 * 5;
+alert(multiplication);
+
+let division = 10 / 2;
+alert(division);
+
+let resto = 15 % 9;
+alert(resto);
+
+let y = 10;
+let z = 5;
+let x = y + z;
+
+let a = 10;
+let b = 5;
+let c = a * b; */
+
+//Iteration#4
+
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]) */
+
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers.splice(0, 1, 'IRONMAN');
+console.log(avengers); */
+
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers.length) */
+
+/* const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters[4]); */
+
+//Iteration#5
+
+/* let number1 = 10;
+let number2 = 20;
+let number3 = 2;
+
+if (number2/number1 == 2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if (number3*5 == number1) {
+  console.log("number3 por 5 es igual a number1");
+}
+
+if (number3*5 == number1 && number1*2 == number2) {
+  console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+if (number2/2 == number1 || number1/5 == number3) {
+  console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+} */
+
+//Iteration#6
+/* for(let i = 0; i < 10; i++){
+    console.log(i);
+} */
+
+/* for(let i = 0; i < 10; i++){
+    if(i%2 == 0){
+        console.log(i)
+    }
+} */
+
+for(let i = 0; i <= 10; i++){
+    if(i == 10){
+        console.log("Dormido!")
+    }else{
+        console.log("intentando dormir");
+    }
+}
